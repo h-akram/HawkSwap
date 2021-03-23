56499a4304ce5967559357406d597714bc537e77
GraceKwon0507 <hkwon11@hawk.iit.edu>
2021-03-23 14:53:41 -0500

    Changed updates

--- sprint-03/code/db-samples/create-user-with-permissions-sample.sql
+++ sprint-03/code/db-samples/create-user-with-permissions-sample.sql
@@ -1,3 +1,6 @@
-GRANT SELECT,INSERT,CREATE TEMPORARY TABLES ON *.* TO worker@'$ACCESSFROMIP' IDENTIFIED BY '$USERPASS';
+-- This file creates a user named worker and passes in the password declared in the environment variables
+-- Depending on the user, you may not want CREATE and DELETE permissions
 
+-- GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,CREATE TEMPORARY TABLES,DROP,INDEX,ALTER ON posts.* TO worker@'$ACCESSFROMIP' IDENTIFIED BY '$USERPASS'; flush privileges;
+GRANT SELECT,INSERT,UPDATE, DELETE, INDEX, ALTER,  DROP, LOCK TABLES, CREATE ON $DATABASENAME.* TO worker@'$ACCESSFROMIP' IDENTIFIED BY '$USERPASS';
 flush privileges;
