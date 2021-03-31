6ea7cd4c206c9bedc44b8dfcdd1c9ec5c4958e5e
hakram <hakram@hawk.iit.edu>
2021-03-31 09:19:49 -0500

    created database

--- polls-test/mysite/mysite/settings.py
+++ polls-test/mysite/mysite/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': BASE_DIR / 'db.mysql',
+        'USER': 'mysqluser',
+        'PASSWORD': 'mysqlpassword',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'CDT'
 
 USE_I18N = True
 
