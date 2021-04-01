a2849d35df0cddc079ba23b8a00f028439e3bc82
Cristian Pintor <cpintor@hawk.iit.edu>
2021-04-01 16:42:50 -0500

    Making additonal changes for database connection

--- sprint-03/code/django/settings.py
+++ sprint-03/code/django/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'wt($cwh_fiadu6t4ry+-^nyr(_3*qd0as6446(6&f)-t%ouykv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
