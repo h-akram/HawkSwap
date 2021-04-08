753246e7ccc2538007437915572231de55e86069
GraceKwon0507 <hkwon11@hawk.iit.edu>
2021-04-08 15:02:44 -0500

    Updated install.md and insert-records.sql

--- sprint-03/build-scripts/provisioner-scripts/post_install_django-install-and-configure-database.sh
+++ sprint-03/build-scripts/provisioner-scripts/post_install_django-install-and-configure-database.sh
@@ -65,6 +65,8 @@ sed -i "s/\$DATABASENAME/$DATABASENAME/g" ~/2021-team06r/sprint-03/code/db-sampl
 sudo mysql -u root < ~/2021-team06r/sprint-03/code/db-samples/create-database.sql
 # This script will create the tables under posts in the mariadb server
 sudo mysql -u root < ~/2021-team06r/sprint-03/code/db-samples/create-table.sql
+# This script will insert records under posts in the mariadb server
+sudo mysql -u root < ~/2021-team06r/sprint-03/code/db-samples/insert-records.sql
 # This script will create the non-root user named worker and the user for replication
 sudo mysql -u root < ~/2021-team06r/sprint-03/code/db-samples/create-user-with-permissions-sample.sql
 
